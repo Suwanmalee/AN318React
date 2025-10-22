33972222f4347c31fb53949fc0feadef8d32c074
Suwanmalee <65111505@dpu.ac.th>
2025-10-23 01:17:35 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-website/sw.js', { scope: '/my-website/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AN318REACT/sw.js', { scope: '/AN318REACT/' })})}
